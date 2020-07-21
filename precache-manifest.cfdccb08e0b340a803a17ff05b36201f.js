c60f08ae6dd711a4106839aebd89b4cd92f1b249
yyhk123 <minoook@hotmail.com>
2020-07-21 00:04:04 -0500

    Updates

---
+++ precache-manifest.cfdccb08e0b340a803a17ff05b36201f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8414a567b49b6930329188409909b954",
+    "url": "/crud/index.html"
+  },
+  {
+    "revision": "b6edfd6dd691818621a8",
+    "url": "/crud/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "371f5535c4ad7c665192",
+    "url": "/crud/static/js/2.6062bca5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crud/static/js/2.6062bca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6edfd6dd691818621a8",
+    "url": "/crud/static/js/main.30aa1409.chunk.js"
+  },
+  {
+    "revision": "3d2107f09fcddc21becb",
+    "url": "/crud/static/js/runtime-main.49a9eee0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crud/static/media/logo.5d5d9eef.svg"
+  }
+]);
